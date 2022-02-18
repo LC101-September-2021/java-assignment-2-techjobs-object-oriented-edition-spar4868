98a9ad11be1594bacf0019c04ea039a621b364e1
spar4868 <65833402+spar4868@users.noreply.github.com>
2022-02-17 22:06:20 -0600

    Add files via upload

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,39 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    @Test
+    public void testSettingJobId(){
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertFalse(job1.equals(job2));
+        assertTrue(job1.getId() == job2.getId() - 9 );
 
+        assertTrue(job1.getName().equals("Product tester"));
+        assertTrue(job1.getEmployer().getValue().equals("ACME"));
+        assertTrue(job1.getLocation().getValue().equals("Desert"));
+        assertTrue(job1.getPositionType().getValue().equals("Quality control"));
+        assertTrue(job1.getCoreCompetency().getValue().equals("Persistence"));
+
+    }
+
+    @Test
+    public void testToString(){
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(job3.toString() instanceof String);
+        assertTrue(job3.toString().equals("ID: 0\nName: Product tester\nEmployer: ACME\nLocation: Desert\nPosition Type: Quality control\nCore Competency: Persistence"));
+        Job job4 = new Job();
+        assertTrue(job4.toString().equals("ID: 0\nName: Data not available\nEmployer: Data not available\nLocation: Data not available\nPosition Type: Data not available\nCore Competency: Data not available"));
+    }
 }
